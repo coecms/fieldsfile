9d32fbb456a50962618359d7e1b314b341b77ae6
Scott Wales <scott.wales@unimelb.edu.au>
2013-01-24 17:19:07 +1100

    printstash doesn't need a stashcode as input

--- stash.c
+++ stash.c
@@ -9,12 +9,6 @@ int main(int argc, char ** argv){
     assert(argc == 3);
 
     struct FieldsFile * ff = OpenFieldsFile(argv[1]);
-    int64_t stash = 0;
-    int matches = sscanf(argv[2],"%lld",&stash);
-    if (matches != 1){
-        perror(argv[2]);
-        exit(-1);
-    }
 
     unsigned int uniqueHeight = 0;
     for (size_t i=0; i< ff->header->field_count; ++i){
